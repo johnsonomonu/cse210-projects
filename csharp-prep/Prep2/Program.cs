41be3e8c5a9cf218af54e286d90d4f390c512274
johnsonomonu <johnsonomonu144@gmail.com>
2023-09-21 21:28:24 +0100

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter grade in percentage");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+        string letter = "";
+
+        if(percentage>=90)
+        {
+            letter = "A";
+
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+
+        }
+
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+
+        }
+
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+
+        }
+
+
+        else if (percentage <60)
+        {
+            letter = "F";
+
+        }
+
+        else 
+        {
+            Console.WriteLine("You entered an invalid number");
+
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percentage >=70)
+        {
+            Console.WriteLine("Congratulations you passed");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry to inform you sir grader, you faild whofully");
+
+        }
+
     }
 }
